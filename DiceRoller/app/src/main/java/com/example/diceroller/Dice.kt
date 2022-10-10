40e4c39ad9433c6eb2857871970396893e9285ac
Mateo PiskaC <mateopiskac2@gmail.com>
2022-10-10 20:49:34 +0200

    Added button Dice roll method

--- DiceRoller/app/src/main/java/com/example/diceroller/Dice.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/Dice.kt
@@ -1,7 +1,15 @@
 package com.example.diceroller
 
-class Dice {
-    var sides = 6
-
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
+}
+fun main() {
+    val myFirstDice = Dice(6)
+    println("Your ${myFirstDice.numSides} sided dice just rolled ${myFirstDice.roll()}!")
+    val mySecondDice = Dice(20)
+    print("Your ${mySecondDice.numSides} sided dice just rolled ${mySecondDice.roll()}!")
+
+
 }
